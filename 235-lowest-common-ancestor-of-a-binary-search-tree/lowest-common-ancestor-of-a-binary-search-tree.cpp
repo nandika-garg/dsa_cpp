f209010f09db192c3d7b85617c9ea7e0925009c8
Nandika Garg <nandikagarg10@gmail.com>
2026-08-04 09:48:24 +0530

    Time: 21 ms (72.53%) | Memory: 23.4 MB (66.75%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,7 +15,5 @@ public:
         if( p->val<root->val && q->val<root->val) return lowestCommonAncestor(root->left,p,q);
         if( p->val>root->val && q->val>root->val) return lowestCommonAncestor(root->right,p,q);
         return root;
-        
-        
     }
 };
