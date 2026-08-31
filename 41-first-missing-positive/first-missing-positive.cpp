9da98fe147ea16a2b71d7cf3e980bda57938c3c4
Nandika Garg <nandikagarg10@gmail.com>
2026-08-31 23:44:43 +0530

    Time: 3 ms (52.09%) | Memory: 57.7 MB (17.65%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> arr(n);
+        for (int i=0; i<n; i++){
+            if (nums[i]>0 && nums[i]<=n){
+                arr[nums[i]-1]=-1;
+            } 
+        }
+        for (int i=0; i<n; i++){
+            if (arr[i]==0) return i+1;
+        }
+       return n+1;
+    }
+};
