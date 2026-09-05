6923214729ccdf400adce0d9db1a53e173c479f6
Nandika Garg <nandikagarg10@gmail.com>
2026-09-05 09:01:31 +0530

    Time: 15 ms (39.37%) | Memory: 202.5 MB (85.02%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+         int n=nums.size();
+        vector<int> minpost(n);
+        int maxval=INT_MIN;
+        int minval=INT_MAX;
+        
+        for (int i=n-1; i>=0; i--){
+            minval=min(nums[i], minval);
+            minpost[i]=minval;
+        }
+        for(int i=0; i<n; i++){
+            maxval=max(nums[i], maxval);
+            if (maxval-minpost[i]<=k) return i;
+        }
+        return -1;
+        
+    }
+};
